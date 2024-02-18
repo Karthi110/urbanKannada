d854a1281a10ab17a888e7f50fdd29a9e67d51b9
karthik <karthi91480@gmail.com>
2024-02-18 12:08:36 +0530

    style changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className=" bg-gradient-to-t from-green-50/20  to-emerald-50 h-fit py-2 px-14 font-bold text-2xl text-slate-700 z-50 sticky top-0 inset-0 select-none ">
+    <nav className=" bg-gradient-to-t from-transparent to-emerald-100 h-fit py-2 px-14 font-bold text-2xl text-slate-700 z-50 sticky top-0 inset-0 select-none ">
       Urbanಕನ್ನಡ
     </nav>
   );
